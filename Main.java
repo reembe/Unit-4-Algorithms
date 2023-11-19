d1cd43e3f767fcfa4d44ad4be4ae0838d8156de2
Reem Belafkih <130573894+reembe@users.noreply.github.com>
2023-11-19 11:46:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         // test case set # 1
-        System.out.println(Algorithms.Alg7("Pizza Pie Party", "Pie"));
-        System.out.println(Algorithms.Alg8("Helloworld"));
-        System.out.println(Algorithms.Alg9("wow"));
-        System.out.println(Algorithms.Alg10("cornonthecobcorncorn", "co"));
-
+        System.out.println(Algorithms.Alg1(12345));
+        System.out.println(Algorithms.Alg2(4,2));
+        System.out.println(Algorithms.Alg3(7,12));
+        System.out.println(Algorithms.Alg4(12,50));
+        System.out.println(Algorithms.Alg5(554));
+        System.out.println(Algorithms.Alg6(200));
         System.out.println("  ");
 
-        // test case set # 2
-        System.out.println(Algorithms.Alg7("Kirby Eats Cake", "WaddleDee"));
-        System.out.println(Algorithms.Alg8("garfieldthecat"));
-        System.out.println(Algorithms.Alg9("applesauce"));
-        System.out.println(Algorithms.Alg10("aroundtheworldaroundtheworldaroundtheworldaroundthe", "world"));
-        }
+        //test case set #2
+        System.out.println(Algorithms.Alg1(45675));
+        System.out.println(Algorithms.Alg2(11,6));
+        System.out.println(Algorithms.Alg3(1,10));
+        System.out.println(Algorithms.Alg4(0,78));
+        System.out.println(Algorithms.Alg5(88));
+        System.out.println(Algorithms.Alg6(25));
     }
+}
