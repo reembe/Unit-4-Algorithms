865adf2a598420f201f5f71f99afc9af6157d0d0
Reem Belafkih <130573894+reembe@users.noreply.github.com>
2023-11-19 11:47:13 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,3 +18,22 @@ public class Main {
         System.out.println(Algorithms.Alg6(25));
     }
 }
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//         // test case set # 1
+//         System.out.println(Algorithms.Alg7("Pizza Pie Party", "Pie"));
+//         System.out.println(Algorithms.Alg8("Helloworld"));
+//         System.out.println(Algorithms.Alg9("wow"));
+//         System.out.println(Algorithms.Alg10("cornonthecobcorncorn", "co"));
+
+//         System.out.println("  ");
+
+//         // test case set # 2
+//         System.out.println(Algorithms.Alg7("Kirby Eats Cake", "WaddleDee"));
+//         System.out.println(Algorithms.Alg8("garfieldthecat"));
+//         System.out.println(Algorithms.Alg9("applesauce"));
+//         System.out.println(Algorithms.Alg10("aroundtheworldaroundtheworldaroundtheworldaroundthe", "world"));
+//         }
+//     }
