903e24b3dfdc945ef65686c0fd1758abfeadd336
Reem Belafkih <130573894+reembe@users.noreply.github.com>
2023-11-19 10:39:09 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // test case set # 1
+        System.out.println(Algorithms.Alg1(12345));
+        System.out.println(Algorithms.Alg2(4,2));
+        System.out.println(Algorithms.Alg3(7,12));
+        System.out.println(Algorithms.Alg4(12,50));
+        System.out.println(Algorithms.Alg5(554));
+        System.out.println(Algorithms.Alg6(200));
+        System.out.println("  ");
+
+        //test case set #2
+        System.out.println(Algorithms.Alg1(45675));
+        System.out.println(Algorithms.Alg2(12,6));
+        System.out.println(Algorithms.Alg3(1,10));
+        System.out.println(Algorithms.Alg4(0,78));
+        System.out.println(Algorithms.Alg5(88));
+        System.out.println(Algorithms.Alg6(62));
+        }
+    }
 
